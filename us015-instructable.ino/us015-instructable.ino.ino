707f446a45d66614415d700d7c68d372d650ed13
Alok Regmi <36977610+dragarok@users.noreply.github.com>
2019-06-22 09:07:08 +0545

    --

---
+++ us015-instructable.ino/us015-instructable.ino.ino
@@ -0,0 +1,35 @@
+// Code for Ultrasonic Distance Module US-015
+unsigned int EchoPin = 2;
+unsigned int TrigPin = 3;
+unsigned long Time_Echo_us = 0;
+//Len_mm_X100 = length*100
+unsigned long Len_cm_X100 = 0;
+unsigned long Len_Integer = 0; //
+unsigned int Len_Fraction = 0;
+void setup()
+{
+Serial.begin(9600);
+pinMode(EchoPin, INPUT);
+pinMode(TrigPin, OUTPUT);
+}
+void loop()
+{
+digitalWrite(TrigPin, HIGH);
+delayMicroseconds(50);
+digitalWrite(TrigPin, LOW);
+Time_Echo_us = pulseIn(EchoPin, HIGH);
+if((Time_Echo_us < 60000) && (Time_Echo_us > 1))
+{
+Len_cm_X100 = (Time_Echo_us*34)/2;
+Len_Integer = Len_cm_X100/1000;
+Len_Fraction = Len_cm_X100%1000;
+Serial.print("Present Length is: ");
+Serial.print(Len_Integer, DEC);
+Serial.print(".");
+if(Len_Fraction < 10)
+Serial.print("0");
+Serial.print(Len_Fraction, DEC);
+Serial.println("cm");
+delay(1000);
+}
+}
